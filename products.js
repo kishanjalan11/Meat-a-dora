be236f3ed29adf41dfacdea5bb1ca9f197c412c3
kishanjalan01 <kishanjalan01@gmail.com>
2023-03-31 00:49:00 +0530

    day 3

--- products.js
+++ products.js
@@ -1,37 +1,384 @@
 let products=[
     {
-        name:"Freshwater/ White Prawns/ Jhinga/ Kolambi (Medium) ",
-        category:"iftar",
-        subCategory:"none",
-        price:"429",
-        quantity:"250gms"
+        id:"1",
+        name:"Chicken Thigh Boneless",
+        category:"Chicken",
+        subCategory:"Bonless & Mince",
+        special:"iftar",
+        price:"339",
+        quantity:"450gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/ffd4144f-25a7-6f26-2d90-9a9db0332dda/original/Chicken_Thigh_Boneless_Hero_Shot.jpg"
     },
     {
-        name:"Freshwater/ White Prawns/ Jhinga/ Kolambi (Medium) ",
-        category:"iftar",
-        subCategory:"none",
+        id:"2",
+        name:"Freshwater/ White Prawns/ Jhinga/ Kolambi (Medium)",
+        category:"Prawns",
+        subCategory:"Medium Size",
+        special:"iftar",
         price:"429",
-        quantity:"250gms"
+        quantity:"250gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/f72684bf-55b0-27a4-0d0f-6bd0535450ff/original/WhatsApp_Image_2022-04-13_at_2.16.31_PM.jpeg"
     },
     {
-        name:"Freshwater/ White Prawns/ Jhinga/ Kolambi (Medium) ",
-        category:"iftar",
-        subCategory:"none",
-        price:"429",
-        quantity:"250gms"
+        id:"3",
+        name:"Chicken Boneless Cubes",
+        category:"Chicken",
+        subCategory:"Boneless & Mince",
+        special:"iftar",
+        price:"339",
+        quantity:"450gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/7b633c75-c203-6c03-5234-9fd75467133a/original/Chicken-Boneless---Tikka-Cut--Hero-Shot_(1).jpg"
     },
     {
-        name:"Freshwater/ White Prawns/ Jhinga/ Kolambi (Medium) ",
-        category:"iftar",
-        subCategory:"none",
-        price:"429",
-        quantity:"250gms"
+        id:"4",
+        name:"Afghani Murgh Seekh Kebab",
+        category:"Ready to Cook",
+        subCategory:"Kebabs & Tandoor",
+        special:"iftar",
+        price:"269",
+        quantity:"4 Pieces",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/44f90ab6-ee35-f696-7713-354e52e6fae5/original/Afghani-Murgh-Seekh-Kebab-uncooked.jpg"
     },
     {
-        name:"Freshwater/ White Prawns/ Jhinga/ Kolambi (Medium) ",
-        category:"iftar",
-        subCategory:"none",
-        price:"429",
-        quantity:"250gms"
+        id:"5",
+        name:"Creamy Afghani Chicken (Mini)",
+        category:"Chicken",
+        subCategory:"Boneless & Mince",
+        special:"iftar",
+        price:"179",
+        quantity:"225gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/777b4969-9cfa-3dff-2108-b1dad1d5f091/original/Creamy-Afghani-Chicken_(1).jpg"
+    },
+
+
+    // ------------------------------------------------------------------------------------------------------------------------------------
+
+
+    {
+        id:"6",
+        name:"Chicken Curry Cut - Small Pieces",
+        category:"Chicken",
+        subCategory:"Curry Cuts",
+        special:"bestsellers",
+        price:"179",
+        quantity:"500gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/ac53a507-4bdd-92cd-ee0b-c96281e04999/original/chicken-curry-cut.jpg?format=webp"
+    },
+    {
+        id:"7",
+        name:"Chunky Butter Chicken Spread",
+        category:"Spreads",
+        subCategory:"Chicken",
+        special:"bestsellers",
+        price:"219",
+        quantity:"1 Piece",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/4f9c5ce8-cd46-b4eb-a0b1-8c7fa8d4280d/original/butterchicken.png"
+    },
+    {
+        id:"8",
+        name:"Chicken Breast - Boneless",
+        category:"Chicken",
+        subCategory:"Boneless & Mince",
+        special:"bestsellers",
+        price:"295",
+        quantity:"450gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/fcea4075-0ed2-23c1-2b3f-1cddcbd1d11f/original/Chicken-Breast-Boneless-(3-4-Pieces)-Hero-Shot_(1).jpg?format=webp"
+    },
+    {
+        id:"9",
+        name:"Rohu (Rui) Medium - Bengali Cut, No Head",
+        category:"Fish & Seafood",
+        subCategory:"Freshwater",
+        special:"bestsellers",
+        price:"289",
+        quantity:"500gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/feedc98d-42e1-0e0a-7da0-0072fa84977c/original/p2_tile_images_6th_folder-09_(1).jpg?format=webp"
+    },
+    {
+        id:"10",
+        name:"Goat Curry Cut - Small Pieces",
+        category:"Mutton",
+        subCategory:"Curry Cuts",
+        special:"bestsellers",
+        price:"599",
+        quantity:"500gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/21c0b2a6-689f-f6ed-3677-a15852624d0c/original/Rich-Goat-Curry-Cut-(Small---19-to-21-Pieces)--Hero-Shot.jpg?format=webp"
+    }
+
+
+    // ------------------------------------------------------------------------------------------------------------------------------------
+
+
+    ,
+    {
+        id:"11",
+        name:"Goat Mince - Combo",
+        category:"Mutton",
+        subCategory:"Combos",
+        special:"combos",
+        price:"1578",
+        quantity:"900gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/8b51398b-b07f-1a84-cce2-0c3ec19b153e/original/Goat---Mince.jpg?format=webp"
+    },
+    {
+        id:"12",
+        name:"Chicken Curry Cut and Chicken Curry Cut",
+        category:"Chicken",
+        subCategory:"Combos",
+        special:"combos",
+        price:"358",
+        quantity:"500gms+500gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/ProductMerchantdising/60f91ea4-e9c9-9ac0-9825-3ce4cb507515/original/1598881046.5939--2020-08-3119_07_26--472.jpeg"
+    },
+    {
+        id:"13",
+        name:"Assorted Kebab Platter",
+        category:"Ready to Cook",
+        subCategory:"Combos",
+        special:"combos",
+        price:"1043",
+        quantity:"720 gms",
+        img:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_15ckn5pzo5x/11/prod_display_image/1634617077.5311--2021-10-1909:47:57--1818"
+    },
+    {
+        id:"14",
+        name:"Chicken Kebab & Biryani Combo",
+        category:"Biryani & Kebab",
+        subCategory:"Combos",
+        special:"combos",
+        price:"468",
+        quantity:"5 Pieces",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/467fc795-de40-e455-e8b2-2270b039dd5c/original/combo.jpg?format=webp"
+    },
+    {
+        id:"15",
+        name:"Chicken Curry Cut (Small Pcs) and Chicken Breast Boneless",
+        category:"Chicken",
+        subCategory:"Combos",
+        special:"combos",
+        price:"474",
+        quantity:"500gms+450gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/ProductMerchantdising/1ebe21bf-dd69-7194-a13d-ab0e85704d1b/original/1598524707.9636--2020-08-2716_08_27--472.jpeg"
+    },
+    
+    
+    // ------------------------------------------------------------------------------------------------------------------------------------
+    
+    
+    {
+        id:"16",
+        name:"Fish Tikka (Mini)",
+        category:"Ready to Cook",
+        subCategory:"Gourmet Marinades",
+        special:"lower",
+        price:"189",
+        quantity:"175gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/ccf4dc1a-1ef3-62e1-18e3-7aa719861427/original/two.jpg?format=webp"
+    },
+    {
+        id:"17",
+        name:"Chunky Butter Chicken Spread",
+        category:"Spreads",
+        subCategory:"Chicken",
+        special:"lower",
+        price:"199",
+        quantity:"1 Piece",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/4f9c5ce8-cd46-b4eb-a0b1-8c7fa8d4280d/original/butterchicken.png"
+    },
+    {
+        id:"18",
+        name:"Sichuan Chilli Chicken (Mini)",
+        category:"Ready to Cook",
+        subCategory:"Gourmet Marinades",
+        special:"lower",
+        price:"199",
+        quantity:"250 gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/9f8ebefe-9a70-49c8-efa0-9ce9838b446a/original/Sichuan-Chilli-Chicken.jpg?format=webp"
+    },
+    {
+        id:"19",
+        name:"Piri Piri Cheese Chicken Burger Patty",
+        category:"Ready to Cook",
+        subCategory:"Burger Patties",
+        special:"lower",
+        price:"199",
+        quantity:"2 Pieces",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/f7cc0f34-c712-12ea-0aec-cd951f8ef51e/original/PDP_peri_peri_burger.jpg?format=webp"
+    },
+    {
+        id:"20",
+        name:"Peppery Chicken Salami (Mini)",
+        category:"Cold Cuts",
+        subCategory:"Salami",
+        special:"lower",
+        price:"199",
+        quantity:"125gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/ad60f59b-612e-11b8-80f4-3308430cc609/original/Peppery-Chicken-Salami_(1).jpg?format=webp"
+    },
+    
+    
+    // ------------------------------------------------------------------------------------------------------------------------------------
+    
+    
+    {
+        id:"21",
+        name:"Chunky Butter Chicken Spread",
+        category:"Spreads",
+        subCategory:"Chicken",
+        special:"breakfast",
+        price:"199",
+        quantity:"1 Piece",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/4f9c5ce8-cd46-b4eb-a0b1-8c7fa8d4280d/original/butterchicken.png"
+    },
+    {
+        id:"22",
+        name:"Peppery Chicken Salami",
+        category:"Cold Cuts",
+        subCategory:"Salami",
+        special:"breakfast",
+        price:"325",
+        quantity:"200gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/ce308f6a-289a-4847-135a-48593775b8c8/original/Peppery-Chicken-Salami-1.jpg?format=webp"
+    },
+    {
+        id:"23",
+        name:"Chunky Shawarma Chicken Spread",
+        category:"Spreads",
+        subCategory:"Chicken",
+        special:"breakfast",
+        price:"219",
+        quantity:"1 Piece",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/c0f6f978-0dd8-d825-7386-0d1f593153c0/original/Shawarma-Chicken.png?format=webp"
+    },
+    {
+        id:"24",
+        name:"Chicken Chorizo Frankfurter",
+        category:"Cold Cuts",
+        subCategory:"Frankfurter",
+        special:"breakfast",
+        price:"299",
+        quantity:"4 Pieces",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/36c60289-829d-830f-d4c7-c62675adb323/original/Frank---Chorizo-unCooked+new.jpg?format=webp"
+    },
+    {
+        id:"25",
+        name:"Peppery Chicken Salami (Mini)",
+        category:"Cold Cuts",
+        subCategory:"Salami",
+        special:"breakfast",
+        price:"199",
+        quantity:"125 gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/ad60f59b-612e-11b8-80f4-3308430cc609/original/Peppery-Chicken-Salami_(1).jpg?format=webp"
+    },
+    
+    
+    // ------------------------------------------------------------------------------------------------------------------------------------
+    
+    
+    {
+        id:"26",
+        name:"Goat Boneless",
+        category:"Mutton",
+        subCategory:"Boneless & Mince",
+        special:"boneless",
+        price:"879",
+        quantity:"500gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/9042ab78-7440-4da6-b903-79cfeb337486/original/Goat-Boneless-Hero-Shot.jpg?format=webp"
+    },
+    {
+        id:"27",
+        name:"Basa - Boneless Cubes",
+        category:"Fish & Seafood",
+        subCategory:"Freshwater",
+        special:"boneless",
+        price:"479",
+        quantity:"400gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/233fa0d5-e6d8-fb39-85b5-959871c2f2c6/original/p0_tile_images-07_(1).jpg?format=webp"
+    },
+    {
+        id:"28",
+        name:"Chicken Breast - Boneless",
+        category:"Chicken",
+        subCategory:"Boneless & Mince",
+        special:"boneless",
+        price:"295",
+        quantity:"450gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/fcea4075-0ed2-23c1-2b3f-1cddcbd1d11f/original/Chicken-Breast-Boneless-(3-4-Pieces)-Hero-Shot_(1).jpg?format=webp"
+    },
+    {
+        id:"29",
+        name:"Seer (Surmai) Medium - Boneless Cubes",
+        category:"Fish & Seafood",
+        subCategory:"Freshwater",
+        special:"boneless",
+        price:"965",
+        quantity:"300gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/7bc75cc8-0bcf-0038-65c2-c0724557803f/original/p1_tile_images-22.jpg?format=webp"
+    },
+    {
+        id:"30",
+        name:"Tilapia (Jalebi) Small - Fillet",
+        category:"Fish & Seafood",
+        subCategory:"Freshwater",
+        special:"boneless",
+        price:"215",
+        quantity:"250gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/ProductMerchantdising/b934c222-75c7-4046-d914-914ca668b37d/original/1596201422.7072--2020-07-3118_47_02--738.jpeg"
+    },
+    
+    
+    // ------------------------------------------------------------------------------------------------------------------------------------
+    
+    
+    {
+        id:"31",
+        name:"Chicken Tenders Boneless",
+        category:"Chicken",
+        subCategory:"Boneless",
+        special:"toprated",
+        price:"289",
+        quantity:"350gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/29ea4091-29c4-6c2d-e980-6789287d691d/original/Chicken-Tenders-(Boneless)-Hero-Shot.jpg?format=webp"
+    },
+    {
+        id:"32",
+        name:"Afghani Murgh Seekh Kebab",
+        category:"Biryan & Kebab",
+        subCategory:"Kebab",
+        special:"toprated",
+        price:"269",
+        quantity:"4 Pieces",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/44f90ab6-ee35-f696-7713-354e52e6fae5/original/Afghani-Murgh-Seekh-Kebab-uncooked.jpg?format=webp"
+    },
+    {
+        id:"33",
+        name:"Chicken Breast Boneless (Large Pack)",
+        category:"Chicken",
+        subCategory:"Boneless",
+        special:"toprated",
+        price:"540",
+        quantity:"900gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/7cb0a040-aaea-f6ed-55a3-7960e4243f54/original/breat.jpg?format=webp"
+    },
+    {
+        id:"34",
+        name:"Tiger Prawns/ Seegadi (Large) - Cleaned & Deveined, With Tail",
+        category:"Fish & Seafood",
+        subCategory:"Seawater",
+        special:"toprated",
+        price:"615",
+        quantity:"250gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/85d6826b-6f2e-259c-358d-86844cf5aad5/original/1626790290.jpg?format=webp"
+    },
+    {
+        id:"35",
+        name:"Basa - Boneless Cubes",
+        category:"Fish & Seafood",
+        subCategory:"Freshwater",
+        special:"toprated",
+        price:"479",
+        quantity:"400gms",
+        img:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/233fa0d5-e6d8-fb39-85b5-959871c2f2c6/original/p0_tile_images-07_(1).jpg?format=webp"
     }
 ];
+localStorage.setItem("products",JSON.stringify(products));
